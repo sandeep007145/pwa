db40b0064c7cc11c869a66b23d3ae846cd41f8ec
rakshana <rakshana.c@spurtreetech.in>
2020-05-08 19:20:46 +0530

    push notification

---
+++ ikona/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB-48o6x0655cqYxiSPsI16PfGpFQMkYAY",
+    authDomain: "ikea-pns.firebaseapp.com",
+    databaseURL: "https://ikea-pns.firebaseio.com",
+    projectId: "ikea-pns",
+    storageBucket: "ikea-pns.appspot.com",
+    messagingSenderId: "491567935703",
+    appId: "1:491567935703:web:8a6fcdfd1fe91337c88092",
+    measurementId: "G-SQGW5VHHG6"
+  }
+};
---
+++ ikona/src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB-48o6x0655cqYxiSPsI16PfGpFQMkYAY",
+    authDomain: "ikea-pns.firebaseapp.com",
+    databaseURL: "https://ikea-pns.firebaseio.com",
+    projectId: "ikea-pns",
+    storageBucket: "ikea-pns.appspot.com",
+    messagingSenderId: "491567935703",
+    appId: "1:491567935703:web:8a6fcdfd1fe91337c88092",
+    measurementId: "G-SQGW5VHHG6"
+  }
+  
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
