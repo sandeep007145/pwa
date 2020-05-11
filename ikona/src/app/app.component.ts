911a52adaa192f68919f3884f9fbe7b4bf819802
Sandeep kumar <sandeepkumar007145@gmail.com>
2020-05-11 09:48:41 -0700

    location

--- ikona/src/app/app.component.ts
+++ ikona/src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+/// <reference types="@types/googlemaps" />
+import { Component, OnInit, ViewChild } from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/messaging';
 import { environment } from 'src/environments/environment';
 import { SwUpdate, SwPush } from '@angular/service-worker';
+// import { } from 'googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
   title = 'ikona';
   displayToken: string;
+  @ViewChild('gmap', {static: true}) gmapElement: any;
+  map: google.maps.Map;
+
+  isTracking = false;
+
+  currentLat: any;
+  currentLong: any;
+
+  marker: google.maps.Marker;
+
+
   constructor(updates: SwUpdate, push: SwPush) {
     updates.available.subscribe(_ => updates.activateUpdate().then(() => {
       console.log('reload for update');
@@ -25,6 +38,15 @@ export class AppComponent{
     }
   }
 
+  ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
+
   permitToNotify() {
     const messaging = firebase.messaging();
     messaging.requestPermission()
@@ -32,6 +54,66 @@ export class AppComponent{
       .catch(err => {
         console.log('Unable to get permission to notify.', err);
       });
+  }
+
+  findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.showPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  showPosition(position) {
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude;
+
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if (!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      this.marker.setPosition(location);
+    }
+  }
+
+  trackMe() {
+    if (navigator.geolocation) {
+      this.isTracking = true;
+      navigator.geolocation.watchPosition((position) => {
+        this.showTrackingPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  showTrackingPosition(position) {
+    console.log(`tracking postion:  ${position.coords.latitude} - ${position.coords.longitude}`);
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude;
+
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if (!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      this.marker.setPosition(location);
+    }
   }
 
   
