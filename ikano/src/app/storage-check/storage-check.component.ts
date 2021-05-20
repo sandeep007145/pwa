c8085f78c6a8c3584a3d32d8100c698c9b019f90
Sandeep Kumar <sandeepkumar007145@gmail.com>
2021-05-20 16:05:20 +0530

    pwa added

--- ikano/src/app/storage-check/storage-check.component.ts
+++ ikano/src/app/storage-check/storage-check.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { StorageService } from '../storage/storage.service';
 
 @Component({
   selector: 'app-storage-check',
@@ -10,9 +9,12 @@ export class StorageCheckComponent implements OnInit {
 
   stores = [];
 
-  constructor(
-    private storageService: StorageService
-  ) { }
+  CACHE_NAME = 'card';
+  TOKEN_KEY = 'cardDetails';
+  FAKE_TOKEN = 'sRKWQu6hCJgR25lslcf5s12FFVau0ugi';
+  FAKE_ENDPOINT = '/card-details';
+
+  constructor() { }
 
   ngOnInit() {
     this.setStorage();
@@ -23,18 +25,17 @@ export class StorageCheckComponent implements OnInit {
 
   setStorage() {
     if (!this.isInStandaloneMode()) {
-      this.storageService.checkLocal(JSON.stringify({ data: 'Working' }));
-      this.storageService.setcache(JSON.stringify({ data: 'Working' }));
+      this.stores.push('Stand Alone Mode(In App): No');
+      this.checkLocal(new Date().toUTCString());
+      this.setcache(new Date().toUTCString());
     }
   }
 
   async getStorage() {
     if (this.isInStandaloneMode()) {
-      this.stores.push('In stand Alone mode');
-      this.stores.push('local: ' + this.storageService.returnLocal());
-      this.stores.push('cache: ' + await this.storageService.displayCachedData());
-    } else {
-      this.stores.push('Not in stand Alone mode');
+      this.stores.push('Stand Alone Mode(In App): Yes');
+      this.returnLocal();
+      this.getCache();
     }
   }
 
@@ -43,6 +44,82 @@ export class StorageCheckComponent implements OnInit {
       (window.matchMedia('(display-mode: standalone)').matches) ||
       // tslint:disable-next-line:no-string-literal
       (navigator['standalone'] === true);
+  }
+
+
+  async setcache(data) {
+    try {
+      const cache = await caches.open(this.CACHE_NAME);
+      this.stores.push('Cache Availble: Available');
+      const responseBody = JSON.stringify({
+        [this.TOKEN_KEY]: data
+      });
+      const response = new Response(responseBody);
+      await cache.put(this.FAKE_ENDPOINT, response);
+      this.stores.push('Endpoint Used in cache: '  +  this.FAKE_ENDPOINT);
+      this.stores.push('Value Stored in cache: ' + data);
+    } catch (error) {
+      this.stores.push('Cache Availble: Not available');
+    }
+  }
+
+  async getCache() {
+    try {
+      const cache = await caches.open(this.CACHE_NAME);
+      this.stores.push('Cache Availble: Available');
+      const response = await cache.match(this.FAKE_ENDPOINT);
+      this.stores.push('Endpoint Used to get cache: '  +  this.FAKE_ENDPOINT);
+      if (!response) {
+        this.stores.push('Cache Found: No');
+        return null;
+      }
+      this.stores.push('Cache Found: Yes');
+      const responseBody = await response.json();
+      this.stores.push('Value Retained From Cache: ' + responseBody[this.TOKEN_KEY]);
+      return responseBody[this.TOKEN_KEY];
+    } catch (error) {
+      console.log('getToken error:', { error });
+    }
+  }
+
+  async clearCache() {
+    try {
+      const cache = await caches.open(this.CACHE_NAME);
+      const response = await cache.match(this.FAKE_ENDPOINT);
+      if (!response) {
+        return null;
+      }
+      cache.delete(this.FAKE_ENDPOINT);
+    } catch (error) {
+      console.log('getToken error:', { error });
+    }
+  }
+
+  async displayCachedData() {
+    const cachedToken = await this.getCache();
+    return cachedToken;
+  }
+
+  checkLocal(data) {
+    try {
+      localStorage.setItem('localStorage', data);
+      this.stores.push('Local Storage Available: Yes');
+      this.stores.push('Key Used In LocalStorage: localStorage');
+      this.stores.push('Value Stored in Local: ' + data);
+    } catch {
+      this.stores.push('Local Storage Available: No');
+    }
+  }
+
+  returnLocal() {
+    try {
+      localStorage.getItem('localStorage');
+      this.stores.push('Local Storage Available: Yes');
+      this.stores.push('Key Used to retain LocalStorage: localStorage');
+      this.stores.push('Value Retained from Local: ' + localStorage.getItem('localStorage'));
+    } catch {
+      this.stores.push('Local Storage Available: No');
+    }
   }
 
 
